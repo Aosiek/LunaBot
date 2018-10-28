a7cd04741b7f0d41868cc0d8765abcda9e2b11ee
Aosiek <aoi_@op.pl>
2018-10-28 20:23:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -134,7 +134,7 @@ client.on('message', message => {
         message.channel.send("https://www.youtube.com/watch?v=SEntHIpylrc");
         break;
             
-        case "-gram w lola":
+        case "-lol":
         message.channel.send("Została dodana rola League of Legends! Powodzenia na Fields of Justice!");
         author.addRole(message.author.guild.roles.find("name", "League of Legends"));
         break;
